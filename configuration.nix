4ca41d6e81e1e0fdce39404c26d44363787fb486
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2025-07-21 21:32:46 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -11,10 +11,10 @@
 
   # Use the GRand Unified Bootloader
   boot.loader = {
-    efi = {
+    	efi = {
 		canTouchEfiVariables = true;
 		efiSysMountPoint = "/boot";
-    };
+    	};
 	grub = {
 		enable = true;
 		efiSupport = true;
@@ -23,18 +23,21 @@
 	};
   };
 
-  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=0" ];
-
   networking = {
-  	hostName = "ts140"; # Define your hostname.
-  	networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  	hostName = "TS140-PC"; # Define your hostname.
+  	networkmanager = {
+	  	enable = true;  # Easiest to use and most distros use this by default.
+		plugins = with pkgs; [
+		  networkmanager-openvpn
+		];
+	}
 	firewall.enable = true;
 	firewall.trustedInterfaces = [ "tailscale0" ];
   	nameservers = [
-    	"1.1.1.1"
-    	"1.0.0.1"
-    	"8.8.8.8"
-    	"8.8.4.4"
+		"1.1.1.1"
+		"1.0.0.1"
+		"8.8.8.8"
+		"8.8.4.4"
   	];
   };
 
@@ -59,15 +62,46 @@
   };
 
   # Enable sound.
+  services.avahi.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-	audio.enable = true;
+    audio.enable = true;
     pulse.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     jack.enable = true;
-	wireplumber.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+	    "default.clock.quantum" = 1024;
+	    "default.clock.min-quantum" = 1024;
+        };
+     };
+     extraConfig.pipewire-pulse."92-low-latency" = {
+      context.modules = [
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            pulse.min.req = "1024/48000";
+            pulse.default.req = "1024/48000";
+            pulse.min.frag = "256/48000";
+            pulse.min.quantum = "256/48000";
+          };
+        }
+      ];
+      stream.properties = {
+        resample.quality = 10;
+      };
+    };
+
+    raopOpenFirewall = true;
+    extraConfig.pipewire."10-airplay" = {
+      "context.modules" = [
+          {
+          name = "libpipewire-module-raop-discover";
+          }
+      ];
+    };
   };
 
   # Enable tailscale
@@ -76,12 +110,12 @@
   # Define the current user.
   users.users.erizur = {
     isNormalUser = true;
-	shell = pkgs.zsh;
-    extraGroups = [ "networkmanager" "wheel" "bluetooth"]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+    extraGroups = [ "networkmanager" "wheel" "bluetooth" "audio" "uinput" ]; # Uinput might be unsafe, but required for some gamepad projects I use.
     home = "/home/erizur";
   };
 
-  # List packages installed in system profile. To search, run:
+  # Flake support
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   
   # Packages
@@ -89,19 +123,14 @@
     git
     neovim
     wget
-	tree
+    tree
 
-	p7zip
-	unzip
-	unrar
+    p7zip unzip unrar
 
     (pkgs.callPackage ./extra/kshift.nix {})
     
-    cmake
-    ninja
-    clang
-    clang-tools
-    lldb
+    cmake ninja
+    clang clang-tools lldb
     direnv
     
     wineWowPackages.staging
@@ -110,11 +139,27 @@
     keyd
     fortune
 
-	jdk17
-	jdk8
-	uutils-coreutils-noprefix
+    libpulseaudio
+    lact
+
+    xdg-utils
+    alsa-utils
+
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
+    gst_all_1.gst-vaapi
+
+    jdk17 jdk8
+    uutils-coreutils-noprefix
   ];
-  environment.variables.EDITOR = "nvim";
+  environment.variables = {
+    EDITOR = "nvim";
+    ROC_ENABLE_PRE_VEGA = "1"; 
+  };
 
   programs.steam = {
   	enable = true;
@@ -143,54 +188,36 @@
   services.blueman.enable = true;
 
   hardware.graphics.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
 
   fonts = {
   	packages = with pkgs; [
-    	noto-fonts
-    	noto-fonts-cjk-sans
-    	noto-fonts-emoji
-    	liberation_ttf
-    	fira-code
-    	fira-code-symbols
-    	mplus-outline-fonts.githubRelease
-    	dina-font
-    	proggyfonts
-    	nerd-fonts.jetbrains-mono
-    	ttf_bitstream_vera
-    	ipafont
-    	kochi-substitute
-    	carlito
-    	hubot-sans
-    	eurofurence
-    	comic-mono
-    	montserrat
-    	helvetica-neue-lt-std
+	    noto-fonts noto-fonts-cjk-sans noto-fonts-emoji liberation_ttf
+	    aileron fragment-mono comic-mono work-sans hubot-sans eurofurence
+	    dosis manrope montserrat helvetica-neue-lt-std
+	    nerd-fonts.fira-code
+	    nerd-fonts.ubuntu
+	    nerd-fonts.hack
+	    nerd-fonts.comic-shanns-mono
+	
+	    carlito dejavu_fonts ipafont kochi-substitute source-code-pro
+	    ttf_bitstream_vera
   	];
 
   	fontconfig = {
 		useEmbeddedBitmaps = true;
 		defaultFonts = {
-     		monospace = [
-			"JetBrainsMono Nerd Font"
-			"IPAGothic"
-     		];
-     		sansSerif = [
-			"Noto Sans"
-			"IPAGothic"
-     		];
-     		serif = [
-			"Noto Serif"
-			"IPAPMincho"
-     		];
+		     monospace = [
+		      "JetBrainsMono Nerd Font"
+		      "IPAGothic"
+		     ];
+		     sansSerif = [
+		      "Noto Sans"
+		      "IPAGothic"
+		     ];
+		     serif = [
+		      "Noto Serif"
+		      "IPAPMincho"
+		     ];
 		};
 	};
   };
@@ -201,18 +228,30 @@
 		enable = true;
   		type = "fcitx5";
   		fcitx5.addons = with pkgs; [
-     		fcitx5-mozc
-     		kdePackages.fcitx5-qt
+     			fcitx5-mozc
+     			kdePackages.fcitx5-qt
   		];
 	};
   };
 
   services.keyd = {
     enable = true;
+    keyboards = {
+	default = {
+	  ids = ["*"];
+	  settings = {
+	    alt = {
+	      "`" = "hiragana";
+              capslock = "muhenkan";
+	      home = "katakanahiragana";
+	    };
+	  };
+	};
+    };
   };
   
   nix.gc.options = "--delete-older-than 7d";
   nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "24.11" # Don't change this unless fully reinstalling
+  system.stateVersion = "25.05" # Don't change this unless fully reinstalling
 }
 
