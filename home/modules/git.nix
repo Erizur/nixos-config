f86e91fbf5ef9c63d4437da7943cf1a061918478
AM_Erizur <sjdks@outlook.com>
2025-07-23 02:12:39 -0500

    Update git to use GCM

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -2,10 +2,18 @@
 {
   programs.git = {
     enable = true;
+    lfs.enable = true;
+    package = pkgs.gitFull;
+
     userName = "AM_Erizur";
     userEmail = "sjdks@outlook.com";
     
     extraConfig = {
+      credential = {
+        helper = "manager";
+        "https://github.com".username = "Erizur";
+        credentialStore = "cache";
+      };
       init.defaultBranch = "main";
     };
   };
