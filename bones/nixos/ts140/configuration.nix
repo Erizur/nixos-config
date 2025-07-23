a168f61520eb7ef45c1bbeb21cf7d73efc5124d8
AM_Erizur <sjdks@outlook.com>
2025-07-23 02:32:34 -0500

    Add OpenCore boot entry

--- bones/nixos/ts140/configuration.nix
+++ bones/nixos/ts140/configuration.nix
@@ -11,4 +11,15 @@
         extraPackages = with pkgs; [ amdvlk rocmPackages.clr.icd mesa ];
         extraPackages32 = with pkgs.pkgsi686Linux; [ amdvlk mesa ];
     };
+
+    boot.loader.grub.extraEntries = ''
+        menuentry "OpenCore" {
+            insmod chain
+            insmod part_gpt
+            insmod fat
+            set root=(hd3,gpt1)
+            chainloader /efi/boot/BOOTx64.efi
+            set root=(hd3,gpt1)/efi
+        }
+    '';
 }
