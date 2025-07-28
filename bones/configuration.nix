d472079353a93b0788b44c8712ddb31a653a21be
AM_Erizur <sajoudks@outlook.com>
2025-07-27 23:30:13 -0500

    Add comic-relief and comic-neue

--- bones/configuration.nix
+++ bones/configuration.nix
@@ -45,7 +45,7 @@
   fonts = {
   	packages = with pkgs; [
 	    noto-fonts noto-fonts-cjk-sans noto-fonts-emoji liberation_ttf
-	    aileron fragment-mono comic-mono work-sans hubot-sans eurofurence
+	    aileron fragment-mono comic-mono comic-neue comic-relief work-sans hubot-sans eurofurence
 	    dosis manrope montserrat helvetica-neue-lt-std mplus-outline-fonts.githubRelease
       fira-code fira-code-symbols
 	    nerd-fonts.fira-code
