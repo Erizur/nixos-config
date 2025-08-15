2d59023e883712854cdf211f33a3e2997e801af8
AM_Erizur <sajoudks@outlook.com>
2025-08-15 15:06:02 -0500

    kitty: update config

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -2,12 +2,13 @@
 {
   programs.kitty = lib.mkForce {
     enable = true;
+    enableGitIntegration = true;
+    themeFile = "tokyo_night_storm";
     settings = {
       confirm_os_window_close = 0;
       enable_audio_bell = false;
       mouse_hide_wait = "1.0";
       window_padding_width = 1;
-      themeFile = "tokyo_night_storm";
       font_family = "JetBrainsMono Nerd Font";
       font_size = 10.0;
       symbol_map = let
