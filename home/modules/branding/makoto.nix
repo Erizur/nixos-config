8d83f9805758f9e5f11bc6b7c94c23b85462a671
AM_Erizur <sajoudks@outlook.com>
2025-08-15 15:17:00 -0500

    branding: update makoto branding

--- home/modules/branding/makoto.nix
+++ home/modules/branding/makoto.nix
@@ -2,7 +2,7 @@
 {
 	programs.zsh.shellAliases.nixcgrs = if pkgs.stdenv.isLinux then "sudo nixos-rebuild switch --flake ~/.nixcfg#ts140" else "sudo darwin-rebuild switch --flake ~/.nixcfg#ts140";
 	programs.fastfetch.settings.logo = {
-		source = "${config.home.homeDirectory}/.nixcfg/img/makoto.jpg";
+		source = "${config.home.homeDirectory}/.nixcfg/img/makoto.png";
 		type = "chafa";
 	};
 }
