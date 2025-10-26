fce24168d7080feb2dce5fbc43b357fce1967e9a
AM_Erizur <sajoudks@outlook.com>
2025-10-26 03:00:48 -0500

    update cursors

--- home/main-user.nix
+++ home/main-user.nix
@@ -59,62 +59,55 @@ in
               cp -r ${cursor-path} $out/share/icons/Oxygen-Zion
               chmod -R u+w $out/share/icons/Oxygen-Zion
               cd $out/share/icons/Oxygen-Zion/cursors
-              ln -sf left_ptr pointer
-              ln -sf left_ptr watch
-              ln -sf left_ptr wait
-              ln -sf half-busy progress
+              for dir in cursors cursors_scalable; do
+                test -d "$out/share/icons/Oxygen-Zion/$dir" || continue
+                cd "$out/share/icons/Oxygen-Zion/$dir"
 
-              # Hands
-              ln -sf pointing_hand hand1
-              ln -sf pointing_hand hand2
-              ln -sf pointing_hand pointer
+                ln -sf wait watch
+                ln -sf half-busy progress
+                ln -sf half-busy left_ptr_watch
 
-              # I-beam / text
-              ln -sf xterm ibeam
-              ln -sf xterm text
+                ln -sf pointing_hand hand1
+                ln -sf pointing_hand hand2
+                ln -sf pointing_hand pointer
 
-              # Movement
-              ln -sf fleur move
-              ln -sf fleur size_all
-              ln -sf fleur all-scroll
+                ln -sf xterm ibeam
+                ln -sf xterm text
 
-              # Forbidden / denied
-              ln -sf forbidden not-allowed
-              ln -sf forbidden crossed_circle
-              ln -sf forbidden circle
-              ln -sf forbidden crossed_circle
-              ln -sf forbidden pirate
+                ln -sf closedhand move
+                ln -sf fleur size_all
+                ln -sf fleur all-scroll
 
-              # Help / question
-              ln -sf help question_arrow
-              ln -sf help whats_this
+                ln -sf forbidden not-allowed
+                ln -sf circle crossed_circle
 
-              # DnD actions
-              ln -sf copy dnd-copy
-              ln -sf link dnd-link
-              ln -sf move dnd-move
-              ln -sf left_ptr dnd-none
+                ln -sf help question_arrow
+                ln -sf help whats_this
 
-              # Resize arrows
-              ln -sf size_ver n-resize
-              ln -sf size_ver s-resize
-              ln -sf size_hor e-resize
-              ln -sf size_hor w-resize
-              ln -sf size_bdiag nw-resize
-              ln -sf size_fdiag ne-resize
-              ln -sf size_fdiag sw-resize
-              ln -sf size_bdiag se-resize
-              ln -sf size_hor col-resize
-              ln -sf size_ver row-resize
-              ln -sf size_hor sb_h_double_arrow
-              ln -sf size_ver sb_v_double_arrow
-              ln -sf size_hor v_double_arrow
+                ln -sf copy dnd-copy
+                ln -sf link dnd-link
+                ln -sf closedhand dnd-move
+                ln -sf closedhand dnd-none
+                ln -sf forbidden dnd-no-drop
 
-              # Misc
-              ln -sf cross crosshair
-              ln -sf plus up_arrow
-              ln -sf split_h sb_h_double_arrow
-              ln -sf split_v sb_v_double_arrow
+                ln -sf size_ver n-resize
+                ln -sf size_ver s-resize
+                ln -sf size_hor e-resize
+                ln -sf size_hor w-resize
+                ln -sf size_bdiag nw-resize
+                ln -sf size_fdiag ne-resize
+                ln -sf size_fdiag sw-resize
+                ln -sf size_bdiag se-resize
+                ln -sf size_hor col-resize
+                ln -sf size_ver row-resize
+                ln -sf size_hor sb_h_double_arrow
+                ln -sf size_ver sb_v_double_arrow
+                ln -sf size_hor v_double_arrow
+
+                ln -sf cross crosshair
+                ln -sf split_h sb_h_double_arrow
+                ln -sf split_v sb_v_double_arrow
+              done          
           '';
         };
   programs.home-manager.enable = true;
